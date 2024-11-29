af71080282cdb601badffa84791c4b2088c96b3a
Annie <annaliseruth.delacruz@gmail.com>
2024-11-29 17:41:39 +0800

    using switch case to validate the user input. Name validation done

--- personal_info_collector.py
+++ personal_info_collector.py
@@ -4,38 +4,58 @@
 
 user_information = {}
 collector = []
-user_input = True
 add_input = True
 
-
-while user_input:
-    name = input("Input your fullname: ")
-    address = input("Input your address: ")
-    number = input("Input your cellphone number(+63): ")
-    email = input("Input your email address: ")
-    birthday = input("Input your date of birth(mm/dd/yyyy): ")
-
-    user_information = {
-        "Full name": name,
-        "Address": address,
-        "Cellphone number": number,
-        "Email address": email,
-        "Birth date": birthday 
-    }
-
-    collector.append(user_information)
-    
-    while add_input:
+def validateInput(user):
+    specialCharacters = ["-", ",", ".", "*", "'", "#"]
+    output = ''
+    #print(user)
+    while True:
         try:
-            choice = input("Do you wish to add another user information?(Yes?No): ")
-            if choice == "yes":
-                break
-            elif choice == "no":
-                user_input = False
-                break
-            else:
-                raise Exception("Invalid Input")
-        except Exception:
-            print(Exception)
+            match user:
+                case "name":
+                    output = input("Input your Full name: ").upper()
+                    for name in output:    
+                        if not name.isalpha() and not name.isspace() and name not in specialCharacters:
+                            raise ValueError("Wrong. Try again.")
+                    break
+        except Exception as exception_:
+            print(exception_)
+def main():
+    user_input = True
+    while user_input:
+        name = validateInput("name")
+        address = input("Input your address: ")
+        number = input("Input your cellphone number(+63): ")
+        email = input("Input your email address: ")
+        birthday = input("Input your date of birth(mm/dd/yyyy): ")
 
-print(collector)
+        user_information = {
+            "Full name": name,
+            "Address": address,
+            "Cellphone number": number,
+            "Email address": email,
+            "Birth date": birthday 
+        }
+
+        collector.append(user_information)
+        
+        while add_input:
+            try:
+                choice = input("Do you wish to add another user information?(Yes?No): ").lower()
+                if choice == "yes":
+                    break
+                elif choice == "no":
+                    user_input = False
+                    break
+                else:
+                    raise Exception("Invalid Input")
+            except Exception:
+                print(Exception)
+
+for index in range(len(collector)):
+    print(collector)
+
+main()
+#with open("./Infomation.txt", "a") as file_handle:
+    #file_handle.write(collector)
